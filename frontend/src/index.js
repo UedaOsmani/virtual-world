73c61a54a60fbe9c40292f4d06396a4f67442680
UedaOsmani <79140398+UedaOsmani@users.noreply.github.com>
2022-06-03 13:46:04 +0200

    new

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Welcome from "./components/welcome";
 import 'bootstrap/dist/css/bootstrap.css';
+import Login from "./components/Login/Login";
 
 import App from "./App";
 
@@ -10,7 +11,7 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
-      <Welcome />
+      <Login />
     </BrowserRouter>
   </StrictMode>,
   rootElement
